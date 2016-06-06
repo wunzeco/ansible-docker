6a9abecb2ca5e7fd8e5d9c2ce0e77c5cc2e7fce7
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-06-06 11:10:46 +0100

    [OI] Add support for centos 7

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,9 +4,6 @@ docker_allow_users = [ 'ogonna' ]
 docker_swarm_image_version = '1.2.0'
 
 %w(
-  apt-transport-https
-  apparmor
-  ca-certificates
   python-pip
   docker-engine
 ).each do |pkg|
@@ -15,22 +12,48 @@ docker_swarm_image_version = '1.2.0'
   end
 end
 
-describe package('lxc-docker') do
-  it { should_not be_installed }
+if os[:family] == 'ubuntu'
+  %w(
+    apt-transport-https
+    apparmor
+    ca-certificates
+  ).each do |pkg|
+    describe package(pkg) do
+      it { should be_installed }
+    end
+  end
+
+  describe package('lxc-docker') do
+    it { should_not be_installed }
+  end
+
+  describe file('/etc/init.d/docker') do
+    it { should_not exist }
+  end
+
+  describe file('/etc/default/docker') do
+    it { should be_file }
+    it { should be_mode 644 }
+  end
+
+end
+
+if os[:family] == 'redhat'
+
+  describe package('epel-release') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/systemd/system/docker.service.d/docker.conf') do
+    it { should be_file }
+    it { should be_mode 644 }
+  end
+
 end
 
 # ensure docker-compose binary is available
 describe command('docker-compose -h') do
     its(:exit_status) { should eq 0 }
-end
-
-describe file('/etc/init.d/docker') do
-  it { should_not exist }
-end
-
-describe file('/etc/default/docker') do
-  it { should be_file }
-  it { should be_mode 644 }
 end
 
 docker_allow_users.each do |u|
